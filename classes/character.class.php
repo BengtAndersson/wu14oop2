b053094534a95a92b14e147a3f23961500f37bdb
BengtAndersson <abengt@live.com>
2015-06-04 13:59:42 +0200

    changes in character.class.php file

--- classes/character.class.php
+++ classes/character.class.php
@@ -3,6 +3,7 @@
 class Character extends Base {
   public $name;
 	public $success = 50;
+  public $level = 1;
   public $tools = array();
 
   public function __construct($name){
@@ -10,41 +11,18 @@ class Character extends Base {
 
   }
 
-    public function pickupRandomTool(&$tools) {
-
-    if (count($this->tools) < 3) {
-     
-      $random_tool_index = rand(0, count($tools)-1);
-      $random_tool = $tools[$random_tool_index];
-      $this->tools[] = $random_tool;
-      array_splice($tools, $random_tool_index, 1);
-    }
-  }
-
-  public function loseRandomTool(&$tools) {
-   
-    if (count($this->tools) > 0) {      
-      $random_tool = rand(0, count($this->tools)-1);
-      $lost_tool = array_splice($this->tools, $random_tool, 1);
-      $tools[] = $lost_tool[0];
-    }
-  }
-
+  public function acceptChallenge($challenge, $players) {
+    return $this->name. " har antagit utmaningen: ".$challenge->description."! ".
+    "<br>".
+    $this->name." får nu bestämma om han ska göra den själv eller i team. "; 
+  }  
   public function carryOutChallenge($challenge, $players) {
     $result = array();
     while (count($players) !== 0) {
-      $result[] = $challenge->playChallenge($players);
+      $result[] = $challenge->playChallenge($players);      
       $round_winner_index = array_search($result[count($result)-1], $players);
       array_splice($players, $round_winner_index, 1);
     }
-
     return $result;
   }
-
-  public function carryOutChallengeWithFriend($challenge, &$players) {
-    return $this->carryOutChallenge($challenge, $players);
-  }
-
-
-
 }
--- classes/team.class.php
+++ classes/team.class.php
@@ -3,13 +3,13 @@
 class Team extends Character {
 
   public $members = array();
-
+/*
   public $strength;
   public $will;
   public $stamina;
   public $speed;
   public $tools = array();
-
+*/
   
   public function __construct($name, $humanPlayer, $computerPlayer) {
     $this->members[] = $humanPlayer;
@@ -22,11 +22,7 @@ class Team extends Character {
     $this->speed = $humanPlayer->speed + $computerPlayer->speed;
 
     //adding tools to the team
-    for ($i=0; $i < count($this->members); $i++) { 
-      for ($j=0; $j < count($this->members[$i]->tools); $j++) { 
-        $this->tools[] = $this->members[$i]->tools[$j];
-      }
-    }
+    $this->tools = $humanPlayer->tools;
 
     //call the parent class __construct   (Player) to set name of team
     parent::__construct($name);
